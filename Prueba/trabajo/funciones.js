cfbb419ac1c7ede1981507246539be74955ef90b
Acuascota <joancts1999@outlook.com>
2019-07-23 14:57:03 -0500

    Portafolio con operaciones

---
+++ Prueba/trabajo/funciones.js
@@ -0,0 +1,6 @@
+function suma(){
+    var n1=parseInt(prompt("Ingresa el primer numero"));
+    var n2=parseInt(prompt("Ingresa el segundo numero"));
+    var s=n1+n2;
+    document.write("la suma es : "+s);
+    }
